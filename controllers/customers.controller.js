629ae38ee1c0b3c3e0c6a056b2e7a288ef7a85e0
521h0481 <521h0481@student.tdtu.edu.vn>
2023-09-22 21:11:18 +0700

    22-9

--- controllers/customers.controller.js
+++ controllers/customers.controller.js
@@ -2,7 +2,11 @@ const {Customers} = require('../models');
 
 const getAllCustomers = async (req, res) => {
     try {
-        const customers = await Customers.findAll();
+        const customers = await Customers.find();
+        // check customers if empty
+        if(!customers){
+            return res.status(404).json({message: "There is no customer"});
+        }
         res.status(200).json(customers);
     } catch (error) {
         res.status(500).json({message: error.message});
@@ -12,10 +16,23 @@ const getAllCustomers = async (req, res) => {
 const getCustomerById = async (req, res) => {
     const id = req.params.id || req.body.id || req.query.id;
     try {
-        const customer = await Customers.findOne({where: {id}});
+        const customer = await Customers.findById(id);
         res.status(200).json(customer);
     } catch (error) {
         res.status(500).json({message: error.message});
+    }
+}
+
+const getCustomerByTel = async (req, res) => {
+    const tel = req.params.tel || req.body.tel || req.query.tel;
+    try {
+        const customer = await Customers.findOne({PhoneNumber: tel});
+        if(!customer){
+            return res.status(404).json({message: "Customer not found"});
+        }
+        res.status(200).json(customer);
+    } catch (error) {
+        
     }
 }
 
@@ -32,5 +49,6 @@ const createCustomer = async (req, res) => {
 module.exports = {
     getAllCustomers,
     getCustomerById,
-    createCustomer
+    createCustomer,
+    getCustomerByTel
 }
--- routers/customers.router.js
+++ routers/customers.router.js
@@ -2,11 +2,23 @@ const express = require('express');
 const customersRouter = express.Router();
 require('dotenv').config();
 
-customersRouter.post('/', /* Your handler here */);
+const {createCustomer, getAllCustomers, getCustomerById, getCustomerByTel} = require('../controllers/customers.controller');
+const {authentication} = require('../middlewares/authentication/authentication');
+const {authorization} = require('../middlewares/authorization/authorization');
+const {isExistId,
+    isCreated,
+    validateInput,
+    isExistEmail,
+    isActive} = require('../middlewares/validations/validation');
+const {Customers} = require("../models");
+
+customersRouter.post('/', authentication, authorization(['employee']), isActive, createCustomer);
 // Lấy danh sách tất cả khách hàng.
-customersRouter.get('/', /* Your handler here */);
+customersRouter.get('/', authentication, authorization(['employee']), isActive, getAllCustomers);
 // Lấy thông tin của một khách hàng cụ thể.
-customersRouter.get(`/:id`, /* Your handler here */);
+customersRouter.get(`/:id`, authentication, authorization(['employee']), isActive, isExistId(Customers), getCustomerById);
+// Lấy thông tin của kh bằng telephone
+customersRouter.get(`/:tel`, authentication, authorization(['employee']), isActive, getCustomerByTel);
 // Cập nhật thông tin của một khách hàng cụ thể.
 customersRouter.put(`/:id`, /* Your handler here */);
 
