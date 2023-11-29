8d233e3926253eb4f86fbf138c7fd1e49199818b
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-11-29 21:12:27 +0700

    ...

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
-const rootRouter = require('./routers');
-const path = require('path');
-const staticPath = path.join(__dirname, 'public');
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 4000;
+const rootRouter = require("./routers");
+const path = require("path");
+const staticPath = path.join(__dirname, "public");
 app.use(cors());
 app.use("/public", express.static(staticPath));
-app.use(express.json());    
-app.use('/api', rootRouter);
+app.use(express.json());
+app.use("/api", rootRouter);
 
 // demo send linkLogin
 app.use(express.static(__dirname));
-const {authenticationLinkLogin} = require('./middlewares/authentication/authentication');
+const {
+  authenticationLinkLogin,
+} = require("./middlewares/authentication/authentication");
 const pageChangePassword = process.env.pageChangePassword;
-app.get('/', (req, res) => {
-  res.redirect('/login.html');
-})
+app.get("/", (req, res) => {
+  res.redirect("/login.html");
+});
 app.get(pageChangePassword, authenticationLinkLogin, (req, res) => {
   const token = req.query.token;
   res.redirect(`/login.html?token=${token}`);
-})
+});
 //
 
-const {MONGO_URL} = process.env;
+const { MONGO_URL } = process.env;
 
-mongoose.connect(MONGO_URL)
-  .then(() => console.log('Connect to mongoDB successfully'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Connect to mongoDB successfully"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.listen(PORT,async () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
