78912db087b43d710153398b7571537441305731
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-11-29 22:00:36 +0700

    update Flag product

--- models/Products.js
+++ models/Products.js
@@ -1,33 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema({
-  Barcode: String,
-  Name: { type: String, required: true },
-  ImportPrice: { type: Number, required: true },
-  RetailPrice: { type: Number, required: true },
-  Category: { type: String, required: true },
-  Quantity: { type: Number, required: true },
-  Image: { type: String, required: true },
-  Flag: { type: Number, default: 1},
-  OrderDetails: [{ type: Schema.Types.ObjectId, ref: 'OrderDetail', required: true }]
-},
+const ProductSchema = new Schema(
   {
-    timestamps: true
-  }, {
+    Barcode: String,
+    Name: { type: String, required: true },
+    ImportPrice: { type: Number, required: true },
+    RetailPrice: { type: Number, required: true },
+    Category: { type: String, required: true },
+    Quantity: { type: Number, required: true },
+    Image: { type: String, required: true },
+    Flag: { type: Number, default: 0 },
+    OrderDetails: [
+      { type: Schema.Types.ObjectId, ref: "OrderDetail", required: true },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+  {
     toJSON: {
-        virtuals: true, 
-        transform: function(doc, ret) {
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.__v;
-            return ret;
-        }
+      virtuals: true,
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
     },
     toObject: { virtuals: true },
-    versionKey: false 
-});
+    versionKey: false,
+  }
+);
 
-const Products = mongoose.model('Product', ProductSchema);
+const Products = mongoose.model("Product", ProductSchema);
 
 module.exports = Products;
