a1b878b2720b23d95c21f39af96f21a68e4ed3c2
Tai521h0481 <521h0481@student.tdtu.edu.vn>
2023-12-06 16:55:03 +0700

    Image Product change to upload from client side

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -66,9 +66,6 @@ const getProductByBarcode = async (req, res) => {
 const createProduct = async (req, res) => {
     let {Name, ImportPrice, RetailPrice, Category, Quantity, Image} = req.body;
     try {
-        if(!Image){
-            Image = "https://res.cloudinary.com/dfxqz0959/image/upload/v1701536638/cloudImageWebNodejs/piture_product/pdu6r9dtlfnshwrvsbqm.jpg";
-        }
         let product = await Products.findOne({Name, ImportPrice, RetailPrice, Category, Image});
         if(product){
             product.Quantity += Quantity;
