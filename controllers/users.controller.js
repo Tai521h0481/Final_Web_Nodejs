dfafa3bfa95189bd0762da94b96d94867c45b413
Tai521h0481 <521h0481@student.tdtu.edu.vn>
2023-12-09 00:22:48 +0700

    .

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -81,7 +81,7 @@ const resendEmail = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    Email = user.Username;
+    Email = user.Email;
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
