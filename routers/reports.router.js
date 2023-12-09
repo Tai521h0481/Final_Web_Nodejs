efaa484cb2497003f6c9e1e5f742fa07c0e176f3
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-12-09 23:43:38 +0700

    update router report

--- routers/reports.router.js
+++ routers/reports.router.js
@@ -1,39 +1,53 @@
-const express = require('express');
-const reportsRouter  = express.Router();
-require('dotenv').config();
+const express = require("express");
+const reportsRouter = express.Router();
+require("dotenv").config();
 
+const {
+  getReportToday,
+  getMonthlyOrderCounts,
+  getDashboardMetrics,
+  getReportYesterday,
+  getReportThisWeek,
+  getReportThisMonth,
+  getReportFromTo,
+} = require("../controllers/reports.controller");
+const {
+  isExistId,
+  isCreated,
+  validateInput,
+  isExistEmail,
+  isActive,
+} = require("../middlewares/validations/validation");
+const {
+  authentication,
+} = require("../middlewares/authentication/authentication");
+const { authorization } = require("../middlewares/authorization/authorization");
 
-const {getReportToday, getMonthlyOrderCounts, getDashboardMetrics,
-    getReportYesterday,
-    getReportThisWeek,
-    getReportThisMonth,
-    getReportFromTo} = require('../controllers/reports.controller');
-const {isExistId,
-    isCreated,
-    validateInput,
-    isExistEmail,
-    isActive} = require('../middlewares/validations/validation');
-const {authentication} = require('../middlewares/authentication/authentication');
-const {authorization} = require('../middlewares/authorization/authorization');
-
-const authMiddleware  = [authentication, authorization(['employee', 'admin']), isActive];
+const authMiddleware = [
+  authentication,
+  authorization(["employee", "admin"]),
+  isActive,
+];
 
 // Lấy báo cáo bán hàng.
-reportsRouter.get('/', /* Your handler here */);
+reportsRouter.get("/" /* Your handler here */);
 // lấy Lấy báo cáo bán hàng trong ngày
-reportsRouter.get('/today', ...authMiddleware , getReportToday);
+reportsRouter.get("/today", ...authMiddleware, getReportToday);
 // lấy Lấy báo cáo bán hàng hôm qua
-reportsRouter.get('/yesterday', ...authMiddleware,getReportYesterday);
+reportsRouter.get("/yesterday", ...authMiddleware, getReportYesterday);
 // lấy Lấy báo cáo bán hàng trong tuần
-reportsRouter.get('/this-week', ...authMiddleware, getReportThisWeek);
+reportsRouter.get("/this-week", ...authMiddleware, getReportThisWeek);
 // lấy Lấy báo cáo bán hàng trong tháng
-reportsRouter.get('/this-month', ...authMiddleware, getReportThisMonth);
+reportsRouter.get("/this-month", ...authMiddleware, getReportThisMonth);
 // lấy Lấy báo cáo bán hàng from - to
-reportsRouter.get('/from-to', ...authMiddleware, getReportFromTo);
+reportsRouter.post("/from-to", ...authMiddleware, getReportFromTo);
 // lấy Lấy báo cáo bán hàng theo tháng
-reportsRouter.get('/monthly-order-counts', ...authMiddleware, getMonthlyOrderCounts);
+reportsRouter.get(
+  "/monthly-order-counts",
+  ...authMiddleware,
+  getMonthlyOrderCounts
+);
 // lấy báo cáo hiện lên dashboard
-reportsRouter.get('/dashboard-metrics', ...authMiddleware, getDashboardMetrics);
-
+reportsRouter.get("/dashboard-metrics", ...authMiddleware, getDashboardMetrics);
 
 module.exports = reportsRouter;
